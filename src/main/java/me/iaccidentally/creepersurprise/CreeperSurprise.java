b505bc99effb96d1a9858b70c853b2cdfa448e38
Zarubinator <bio.hazrd-@hotmail.com>
2013-02-10 00:21:04 -0500

    more config options

--- src/main/java/me/iaccidentally/creepersurprise/CreeperSurprise.java
+++ src/main/java/me/iaccidentally/creepersurprise/CreeperSurprise.java
@@ -52,7 +52,7 @@ public class CreeperSurprise extends JavaPlugin implements Listener
 			for (int i = 0; i < 10; i++)
 			{
 				//final Vector vector = event.getEntity().getLocation().getDirection().setY(i * 0.50);
-				
+
 				Firework firework = (Firework)event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.FIREWORK);
 				FireworkMeta meta = (FireworkMeta)firework.getFireworkMeta();
 				builder = FireworkEffect.builder();
@@ -61,27 +61,113 @@ public class CreeperSurprise extends JavaPlugin implements Listener
 				builder.withFade(org.bukkit.Color.GREEN);
 				builder.trail(trail());
 				builder.flicker(flicker());
-				FireworkEffect effect = builder.build();
-				meta.addEffect(effect);
+				meta.addEffect(effectball());
+				meta.addEffect(effectballlarge());
+				meta.addEffect(effectburst());
+				meta.addEffect(effectcreeper());
+				meta.addEffect(effectstar());
 				meta.setPower(power());
 				//firework.setVelocity(vector);
 				firework.setFireworkMeta(meta);
 			}
 		}
 	}
+
 	public int power()
 	{
 		int power = 0 + (int)(Math.random() * ((getConfig().getInt("power") - 0) + 1));
 		return power;
 	}
+
 	public boolean trail()
 	{
 		boolean trail = getConfig().getBoolean("trail");
 		return trail;
 	}
+
 	public boolean flicker()
 	{
 		boolean flicker = getConfig().getBoolean("flicker");
 		return flicker;
+	}
+
+	public FireworkEffect effectball()
+	{
+		builder = FireworkEffect.builder();
+		boolean effecttype = getConfig().getBoolean("effect ball");
+		if (effecttype == true)
+		{
+			FireworkEffect effect = builder.build();
+			builder.with(FireworkEffect.Type.BALL);
+			return effect;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public FireworkEffect effectballlarge()
+	{
+		builder = FireworkEffect.builder();
+		boolean effecttype = getConfig().getBoolean("effect large_ball");
+		if (effecttype == true)
+		{
+			FireworkEffect effect = builder.build();
+			builder.with(FireworkEffect.Type.BALL_LARGE);
+			return effect;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public FireworkEffect effectburst()
+	{
+		builder = FireworkEffect.builder();
+		boolean effecttype = getConfig().getBoolean("effect burst");
+		if (effecttype == true)
+		{
+			FireworkEffect effect = builder.build();
+			builder.with(FireworkEffect.Type.BURST);
+			return effect;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public FireworkEffect effectcreeper()
+	{
+		builder = FireworkEffect.builder();
+		boolean effecttype = getConfig().getBoolean("effect creeper");
+		if (effecttype == true)
+		{
+			FireworkEffect effect = builder.build();
+			builder.with(FireworkEffect.Type.CREEPER);
+			return effect;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public FireworkEffect effectstar()
+	{
+		builder = FireworkEffect.builder();
+		boolean effecttype = getConfig().getBoolean("effect star");
+		if (effecttype == true)
+		{
+			FireworkEffect effect = builder.build();
+			builder.with(FireworkEffect.Type.STAR);
+			return effect;
+		}
+		else
+		{
+			return null;
+		}
 	}
 }
